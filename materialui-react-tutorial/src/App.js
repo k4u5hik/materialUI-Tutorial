18d2cfa4b231381e669032ab6ba6ba7c6741b2e7
Kaushik Chemburkar <attherate@gmail.com>
2022-06-06 11:33:33 +1000

    SaveIcon startIcon, endIcon

--- materialui-react-tutorial/src/App.js
+++ materialui-react-tutorial/src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from '@material-ui/core/Button';
+import SaveIcon from '@material-ui/icons/Save';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'> 
         <Button
+          startIcon={<SaveIcon />}
+          endIcon={<SaveIcon />}
           onClick={() => {
             alert("clicked");
           }}
